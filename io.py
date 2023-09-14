903e89a92ac3b960781b14795a7d799703be3a5e
Xano55 <piotrmalinowski2007@gmail.com>
2023-09-14 10:30:42 +0200

    Edycja io i stworzenie io2

--- io.py
+++ io.py
@@ -9,3 +9,18 @@ no way!!!!
 # funkcja wyjecia
 print("Joe Biden") # funkcja wyjscia
 print("Sleepy Joe")
+
+a = 5
+b = 3.
+c = "Lo tego"
+d = [5, 3, 8, 4]
+a = 5.6161
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(a))
+
+b = int(b)
+print(type(b))
+print(b)
---
+++ io2.py
@@ -0,0 +1,12 @@
+'''
+Prosty kalkulator
+Python ver 3.11
+14.09.23
+'''
+# wprowadzenie danych
+x = float(input("Podaj x: "))
+y = float(input("Podaj y: "))
+
+#obliczenia
+suma = x + y
+print(suma)
